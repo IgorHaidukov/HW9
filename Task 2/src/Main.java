a702de9d8cabb6a2a1fa7ad194b844cfbb7ab076
IgorHaidukov <kolyhool9@gmail.com>
2024-09-12 19:11:00 +0200

    homework

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+
+        test();
+
+
+    }
+
+    private static void test() {
+        System.out.print("Введите число:");
+        int input = new Scanner(System.in).nextInt();
+        System.out.println(input % 2 == 0);
     }
 }
